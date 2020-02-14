b9c6f02836cd3a4c21bbb41239d102e7d07f248d
mrsimpson <github@beimir.net>
2020-02-14 15:45:30 +0100

    Supoort providing a source url, select backend automaticall

--- src/readJcalFromUrl.js
+++ src/readJcalFromUrl.js
@@ -1,10 +1,18 @@
 const fetch = require('node-fetch')
 const ical = require('ical.js')
 
+const BACKEND = "https://mrsimpson.o6s.io/ical-interceptor" // TODO: Configuration
+
 export default async function readJcalFromUrl(url) {
-    let jCal  
+    if (!url) {
+        throw new Error('Supply a URL at which the ical can be retrieved')
+    }
+    let jCal
+    const proxyUrl = url.match(new RegExp(BACKEND))
+        ? url
+        : BACKEND + '/?url=' + url
     try {
-        const resource = await fetch(url)
+        const resource = await fetch(proxyUrl)
         jCal = ical.parse(await resource.text())
         // jCal = await ical.fromURL(url)
     } catch (err) {
